0d3b6d221efd9aa3e68792efd9a34c44c741c11d
Valery Bergman <bergman.vd@phystech.edu>
2023-04-19 17:50:14 +0300

    Move to multi-module architecture

---
+++ controller_side/src/main/java/com/mipt/controller/controllers/MainController.java
@@ -0,0 +1,67 @@
+package com.mipt.controller.controllers;
+
+import com.mipt.controller.service.City;
+import com.mipt.controller.configuration.BrokerConfiguration;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+@Slf4j
+@RestController
+@RequestMapping("/delivery")
+public class MainController {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Data
+    private static class OrderRequest {
+        String city;
+    }
+
+    /*
+     * This controller redirects POST-requests from different cities to their Rabbit queues.
+     *
+     * Be careful to replace the hyphen with an underscore in compound names.
+     */
+    @PostMapping("/order")
+    @ResponseBody
+    public ResponseEntity<String> RedirectDelivery(@NotNull @RequestBody OrderRequest orderRequest) throws IOException, TimeoutException {
+        String cityName = orderRequest.getCity().toUpperCase();
+        log.info("Get message from city " + cityName);
+        ConnectionFactory factory = new ConnectionFactory();
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            City city = City.of(cityName);
+            if (city.equals(City.CityEnum.MOSCOW)) {
+                log.info("Send message to Moscow queue");
+                channel.basicPublish("", BrokerConfiguration.MOSCOW, null,
+                        cityName.getBytes());
+            } else if (city.equals(City.CityEnum.SAINT_PETERSBURG)) {
+                log.info("Send message to Saint-Petersburg queue");
+                channel.basicPublish("", BrokerConfiguration.SAINT_PETERSBURG, null,
+                        cityName.getBytes());
+            } else {
+                log.warn("Unknown city " + cityName + ". Request denied.");
+                return ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body("This city does not yet have our delivery. Maybe you made a typo? " +
+                                "Be careful to replace the hyphen with an underscore in compound names.");
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
---
+++ controller_side/src/main/java/com/mipt/controller/service/City.java
@@ -0,0 +1,61 @@
+package com.mipt.controller.service;
+
+import lombok.Value;
+
+/*
+ * City Enum. This interface made to realize forward compatibility.
+ * If you want to add new city, write it name in CityEnum.
+ *
+ * Usage:
+ * City city = City.of(cityName);
+ * if (city.equals(City.CityEnum.MOSCOW)) { doSomething(); }
+ */
+public interface City {
+
+    String value();
+
+    @Value
+    class CitySimple implements City {
+
+        String value;
+
+        @Override
+        public String value() {
+            return value;
+        }
+
+    }
+
+    enum CityEnum implements City {
+
+        MOSCOW, SAINT_PETERSBURG;
+
+        public static CityEnum parse(String rawValue) {
+            if (rawValue == null) {
+                throw new IllegalArgumentException("Raw value cannot be null!");
+            }
+            for (CityEnum enumValue : values()) {
+                if (enumValue.name().equalsIgnoreCase(rawValue)) {
+                    return enumValue;
+                }
+            }
+            throw new IllegalArgumentException("Illegal CityEnum type!");
+        }
+
+        @Override
+        public String value() {
+            return name();
+        }
+
+    }
+
+    static City of(String value) {
+        try {
+            return CityEnum.parse(value);
+        }
+        catch (IllegalArgumentException e) {
+            return new CitySimple(value.toUpperCase());
+        }
+    }
+
+}
