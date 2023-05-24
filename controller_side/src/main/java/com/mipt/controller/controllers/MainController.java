aa1d395eb539fdb68577c02b234ce8ffad34d8c5
Valery Bergman <bergman.vd@phystech.edu>
2023-05-24 05:48:21 +0300

    Configure Prometheus

--- controller_side/src/main/java/com/mipt/controller/controllers/MainController.java
+++ controller_side/src/main/java/com/mipt/controller/controllers/MainController.java
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Channel;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -27,6 +26,16 @@ public class MainController {
 
     private static final String MOSCOW = "MOSCOW";
     private static final String SAINT_PETERSBURG = "SAINT_PETERSBURG";
+
+    private final Counter requestsFromMoscow;
+    private final Counter requestsFromSaintPetersburg;
+    private final Counter errors;
+
+    public MainController(MeterRegistry meterRegistry) {
+        requestsFromMoscow = meterRegistry.counter("requests_total");
+        requestsFromSaintPetersburg = meterRegistry.counter("requests_saint_petersburg");
+        errors = meterRegistry.counter("errors");
+    }
 
     @Data
     private static class OrderRequest {
@@ -46,12 +55,15 @@ public class MainController {
         City city = City.of(cityName);
         if (city.equals(City.CityEnum.MOSCOW)) {
             log.info("Send message to Moscow queue");
+            requestsFromMoscow.increment();
             rabbitTemplate.convertAndSend(MOSCOW, cityName);
         } else if (city.equals(City.CityEnum.SAINT_PETERSBURG)) {
             log.info("Send message to Saint-Petersburg queue");
+            requestsFromSaintPetersburg.increment();
             rabbitTemplate.convertAndSend(SAINT_PETERSBURG, cityName);
         } else {
             log.warn("Unknown city " + cityName + ". Request denied.");
+            errors.increment();
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body("This city does not yet have our delivery. Maybe you made a typo? " +
